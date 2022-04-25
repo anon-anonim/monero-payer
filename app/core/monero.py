379bc06c0e86c7a373e27608aa4000fca4633ac4
anon-anonim <anon.anonim@gmail.com>
2022-04-25 15:45:50 +0500

    тестирование создания адресов

--- app/core/monero.py
+++ app/core/monero.py
@@ -5,10 +5,20 @@ from asyncpg.pool import Pool
 
 log = logging.getLogger("monero-payer")
 
+from monero.wallet import Wallet
+
+from monero.backends.jsonrpc import JSONRPCWallet
+
 
 async def get_address(pool: Pool):
     log.debug(f"{pool=}")
-    return "Ще за докер?"
+    w = Wallet(JSONRPCWallet(port=18082))
+    mn_adrs = 'ОСНОВНОЙ АДРЕСС- '+str(w.address())
+    mn_acc = 'АККАУНТ- '+str(w.accounts[0])
+    #new_adrs = 'НОВЫЙ АДРЕСС- '+str(w.accounts[0].new_address())
+    sub_adrs_all = 'СПИСОК АДРЕСОВ НА АККАУНТЕ' + str(w.accounts[0].addresses())
+    amt_sub_adrs = 'КОЛИЧЕСТВО АДРЕСОВ НА АККАУНТЕ - docker-compose up -d' + str(len(w.accounts[0].addresses()))
+    return (mn_adrs, mn_acc, sub_adrs_all, amt_sub_adrs)
 
 
 async def check_transaction(
